335921f120ead1af38acab73c45120762a40f4b7
Yurii Pereverziev <yuripereverzev@gmail.com>
2019-06-30 22:36:01 +0300

    my first commit

---
+++ build/assets/js/app.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  setTimeout(function() {
+    document.querySelector('.greating_picture').classList.add('m--show');
+  }, 1000);
+})();
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJhcHAuanMiXSwic291cmNlc0NvbnRlbnQiOlsiKGZ1bmN0aW9uKCkge1xuICAndXNlIHN0cmljdCc7XG5cbiAgc2V0VGltZW91dChmdW5jdGlvbigpIHtcbiAgICBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcuZ3JlYXRpbmdfcGljdHVyZScpLmNsYXNzTGlzdC5hZGQoJ20tLXNob3cnKTtcbiAgfSwgMTAwMCk7XG59KSgpOyJdLCJmaWxlIjoiYXBwLmpzIn0=
