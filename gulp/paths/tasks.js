18bd1d719612f68840bb7a981998ccb116e0ff1b
Yurii Pereverziev <yuripereverzev@gmail.com>
2019-07-01 18:33:46 +0300

    first commit

---
+++ gulp/paths/tasks.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = [
+  './gulp/tasks/less.js',
+  './gulp/tasks/serve.js',
+  './gulp/tasks/pug.js',
+  './gulp/tasks/watch.js',
+  './gulp/tasks/clean.js',
+  './gulp/tasks/js.foundation.js',
+  './gulp/tasks/css.foundation.js',
+  './gulp/tasks/js.process.js',
+  './gulp/tasks/js.lint.js',
+  './gulp/tasks/imagemin.js',
+  './gulp/tasks/webp.js',
+  './gulp/tasks/copy.image.js',
+  './gulp/tasks/sprite.svg.js'
+];
+
---
+++ gulp/tasks/js.process.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function() {
+  $.gulp.task('js:process', function() {
+    return $.gulp.src($.path.app)
+      .pipe($.gp.sourcemaps.init())
+      .pipe($.gp.concat('app.js'))
+      .pipe($.gp.sourcemaps.write())
+      .pipe($.gp.minify())
+      .pipe($.gulp.dest($.config.root + '/assets/js'))
+  })
+};
